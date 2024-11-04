d638cc30855ca271098126db120a43d9e4def280
Harshvardhan Ranvir Singh <76574548+harshvardhan-ranvir-singh@users.noreply.github.com>
2024-11-04 14:15:25 +0530

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -14,12 +14,7 @@ warnings.simplefilter('ignore')
 
 st.set_page_config(page_title="Dashboard", layout="wide", menu_items = None)
 
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 
 TWO_PERCENT_MARKET_PRICE_CE = 0.0
 TWO_PERCENT_MARKET_PRICE_PE = 0.0
